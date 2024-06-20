7d2ae12c158c475d48f50eb1991750858827a659
VitaliiKyrpach <126057381+VitaliiKyrpach@users.noreply.github.com>
2024-06-20 11:49:38 +0300

    add next two sections

--- js/tailwind-config.js
+++ js/tailwind-config.js
@@ -21,11 +21,20 @@ tailwind.config = {
             colorAccentAdd: 'BCD0E5',
             colorFeat1: '#4D8DFF',
             colorFeat2: '#740A76',
-            colorFeat3: '#F03E3D'
+            colorFeat3: '#F03E3D',
+            colorFeat4: '#FF9900'
         },
         boxShadow: {
-            headerShadow: '0px 2px 6px rgba(0, 0, 0, 0.25)'
+            headerShadow: '0px 2px 6px rgba(0, 0, 0, 0.25)',
+            stuffShadow: '0px 2px 8px rgba(86, 117, 146, 0.1)'
           },
+        backgroundImage: {
+            'back-feature': "url('../img/featureback.png')",
+            'back-stuff': "url('../img/stuffback.png')"
+        },
+        width:{
+            stuffMD: 'calc(408px - 105px)',
+          }
     }
     }
 }
