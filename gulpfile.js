4ed887249d3d037cfa0f8cafebb9d89c6eb6004c
VitaliiKyrpach <126057381+VitaliiKyrpach@users.noreply.github.com>
2024-07-16 10:11:50 +0300

    add gh-pages, finish gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 import gulp from 'gulp'
+import ghPages from 'gulp-gh-pages'
 import {path} from './gulp/config/path.js'
 import { plugins } from './gulp/config/plugins.js'
-// import {copy} from './gulp/tasks/copy.js'
 import { reset } from './gulp/tasks/reset.js'
 import { html } from './gulp/tasks/html.js'
 import { server } from './gulp/tasks/server.js'
@@ -11,7 +11,6 @@ import { js } from './gulp/tasks/js.js'
 global.app = {path, gulp, plugins}
 
 const watcher = () =>{
-    // gulp.watch(path.watch.files, copy);
     gulp.watch(path.watch.html, html);
     gulp.watch(path.watch.images, images);
     gulp.watch(path.watch.js, js);
@@ -22,3 +21,15 @@ const mainTasks = gulp.parallel(html, images, js)
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server))
 
 gulp.task('default', dev)
+gulp.task('deploy', function() {
+    return gulp.src('./dist/**/*',{encoding: false})
+      .pipe(ghPages());
+  });
+
+// const deploy = () =>{
+//     return gulp.src('./dist/**/*',{encoding: false})
+//       .pipe(ghPages());
+// }
+
+// exports.default = dev;
+// exports.deploy = deploy;
