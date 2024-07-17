83e649803565ebb2c43d9a937fedf65cbc64bb73
VitaliiKyrpach <126057381+VitaliiKyrpach@users.noreply.github.com>
2024-07-17 11:33:08 +0300

    Update 2024-07-17T08:33:06.111Z

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -2,18 +2,29 @@
   const refs = {
     openMenuBtn: document.querySelector("[data-menu-open]"),
     closeMenuBtn: document.querySelector("[data-menu-close]"),
-    menuBack: document.querySelector("[data-menu-back]"),
+    menuBack: document.querySelector("[data-type='menuBack']"),
     menu: document.querySelector("[data-menu]"),
   };
 
   refs.openMenuBtn.addEventListener("click", toggleMenu);
   refs.closeMenuBtn.addEventListener("click", toggleMenu);
+  refs.menuBack.addEventListener("click", handleCloseMenu);
 
   function toggleMenu() {
     refs.menuBack.classList.toggle('opacity-100')
     refs.menuBack.classList.toggle('opacity-0')
     refs.menuBack.classList.toggle('pointer-events-none')
     refs.menu.classList.toggle("translate-x-0");
-    document.body.classList.toggle("no-scroll");
+    document.body.classList.toggle("overflow-hidden");
   }
+  
+  function handleCloseMenu(e) {
+    if (
+      e.target.dataset.type == 'menuBack' ||
+      e.target.dataset.type == "login" ||
+      e.target.dataset.type == "register"
+    ) {
+      toggleMenu();
+    }
+  };
 })();
